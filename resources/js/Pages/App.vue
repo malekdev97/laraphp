1cfbd470bf17fb96de5a91555682de03f9597d91
Abdulmalik <omayri970@gmail.com>
2024-08-11 17:21:01 +0300

    update file

--- resources/js/Pages/App.vue
+++ resources/js/Pages/App.vue
@@ -1,8 +1,18 @@
 <template>
-<h1>Vue</h1>
+<h1>Vue {{ name }}</h1>
+<p><Link href="/users">Users</Link></p>
 
 </template>
 <script>
+import { Link } from '@inertiajs/vue3';
 
+export default {
+    props: {
+        name: String
+    },
+    components: {
+        Link
+    }
+}
 
 </script>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return inertia('App');
+    return inertia('App', [
+        'name' => 'John Doe']
+    );
 });
+
+Route::get('/users', function () {
+    return inertia('Users');
+});
+
+Route::get('/settings', function () {
+    return inertia('Settings');
+    }
+);
