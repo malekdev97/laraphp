4ce01ed42bb1c189c67fac2352e299707e673509
Malek <omayri970@gmail.com>
2024-08-20 03:33:33 +0300

    add tailwind

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return inertia('App', [
-        'name' => 'John Doe']
-    );
+    return view('index');
 });
-
-Route::get('/users', function () {
-    return inertia('Users');
-});
-
-Route::get('/settings', function () {
-    return inertia('Settings');
-    }
-);
